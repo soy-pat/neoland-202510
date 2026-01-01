22ba73681226a32989133e1f342148d67a0ab625
soy-pat <pizquierdocortes@gmail.com>
2026-01-01 22:38:21 +0100

    WIP: implement complete and delete issue

--- staff/patricia-izquierdo/playground/issue-tracker/main.jsx
+++ staff/patricia-izquierdo/playground/issue-tracker/main.jsx
@@ -39,6 +39,11 @@ function App() {
         }
     }
 
+    const handleIssueComplete = idIssue => {
+
+        console.log(idIssue)
+    }
+
     const listIssues = []
 
     for (let i = 0; i < issues.length; i++) {
@@ -46,29 +51,36 @@ function App() {
 
         const subjectIssue = issue.subject
         const bodyIssue = issue.body
+        const idIssue = issue.id
 
         const listIssue = <li className="text-xs">
-            <div>
-                <h3 className="font-bold">{subjectIssue}</h3>
-                <p>{bodyIssue}</p>
+            <div className="flex flex-row border p-2 text-xs m-5 justify-between">
+                <div className="flex flex-col">
+                    <h3 className="font-bold text-left">{subjectIssue}</h3>
+                    <p className="text-left">{bodyIssue}</p>
+                </div>
+                <div className="flex flex-col">
+                    <button type="button" onClick={handleIssueComplete(idIssue)}>✅</button>
+                    <button type="button">🗑️</button>
+                </div>
             </div>
         </li>
 
         listIssues.push(listIssue)
     }
 
-    return <div className="flex flex-row place-content-evenly">
-        <div className="flex flex-col align-self text-xl">OPEN
-            <form className="flex flex-col border p-2 w-25 text-xs" onSubmit={handleIssueSubmit}>
+    return <div className="flex flex-row">
+        <div className="flex flex-col text-xl grow basis-0 min-w-0 text-center">OPEN
+            <form className="flex flex-col border p-2 text-xs m-5" onSubmit={handleIssueSubmit}>
 
-                <label htmlFor="subject">Subject</label>
+                <label className="text-left" htmlFor="subject">Subject</label>
                 <input className="border" type="text" id="subject" />
 
-                <label htmlFor="body">Body</label>
+                <label className="text-left" htmlFor="body">Body</label>
                 <textarea className="border h-15" type="text" id="body" />
 
                 <div className="flex justify-end">
-                    <button className="border bg-blue-100 text-white cursor-pointer p-1 m-1 text-xs w-15" type="submit">+ New</button>
+                    <button className="border bg-blue-100 text-white cursor-pointer p-1 m-1 text-xs" type="submit">+ New</button>
                 </div>
 
             </form>
@@ -79,7 +91,7 @@ function App() {
                 </ul>
             </div>
         </div>
-        <div className="flex flex-col align-self text-xl">CLOSED
+        <div className="flex flex-col text-xl grow basis-0 min-w-0 text-center">CLOSED
             <div></div>
         </div>
     </div>
